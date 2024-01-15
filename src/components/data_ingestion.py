ab93103b919eddad6a39f0f18c83189f49b00781
rohitdutta13 <rohitdutta13@gmail.com>
2024-01-15 16:27:42 -0600

    App deployment completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,6 @@
 import os, sys
 from src.constants import *
 from src.config.configuration import *
-import os, sys
 import pandas as pd 
 import numpy as np 
 from sklearn.model_selection import train_test_split
